bebe0779a002b11a0bcc9b580f49d1d29d2b54fc
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2026-05-04 18:30:40 +0530

    fix: Update FunctionalTests to use Cucumber 6.x API (io.cucumber)

--- stockmanager/src/test/java/functional/FunctionalTests.java
+++ stockmanager/src/test/java/functional/FunctionalTests.java
@@ -1,7 +1,7 @@
 package functional;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "classpath:features/",
         plugin = "html:build/reports/cucumber",
         glue = "functional",
-        strict = true)
+        dryRun = false)
 public class FunctionalTests {
 
 }
