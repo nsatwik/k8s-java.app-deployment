f3e6e0c99067e63f75eb1da54e2794d264db77f6
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2026-05-04 18:30:17 +0530

    fix: Update RestStepDefs to use Cucumber 6.x API (io.cucumber)

--- stockmanager/src/test/java/functional/RestStepDefs.java
+++ stockmanager/src/test/java/functional/RestStepDefs.java
@@ -1,14 +1,12 @@
 package functional;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.junit.runner.RunWith;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.co.danielbryant.djshopping.stockmanager.StockManagerApplication;
 import uk.co.danielbryant.djshopping.stockmanager.model.Stock;
 
@@ -20,7 +18,6 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 
 @ContextConfiguration
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = StockManagerApplication.class)
 public class RestStepDefs {
 
